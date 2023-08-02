83384789a6657d6693d215adb0d573039d810677
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-08-02 20:23:02 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -145,7 +145,7 @@ USE_TZ = False
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = "http://3.38.180.187/media/"
+MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
