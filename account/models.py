8cc308083483b61a86d55f9847005a8e56c52ca2
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-08-12 13:55:58 +0900

    얼굴인식

--- account/models.py
+++ account/models.py
@@ -4,6 +4,6 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     nickname = models.CharField(max_length=100)
-    image = models.ImageField(upload_to="user")
+    image = models.ImageField(upload_to="user",null = True)
     face_registered = models.BooleanField(default=False)
     mode = models.CharField(max_length=100,default = "normal")
