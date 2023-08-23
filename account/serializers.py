2171f2323d88ee19a9cb2045f1bdbc59c9d8adad
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-08-24 04:44:47 +0900

    유저 사진

--- account/serializers.py
+++ account/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import User
+from django.conf import settings
 
 class UserSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
     class Meta:
         model = User
-        fields = ["username","nickname","face_registered","mode"]
+        fields = ["username","nickname","face_registered","mode",'image']
+
+    def get_image(self,obj):
+        return settings.PROJECT_HOST + obj.image.url
--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-PROJECT_HOST = "http://13.124.28.244/"
+PROJECT_HOST = "http://3.36.95.105"
 # Application definition
 
 INSTALLED_APPS = [
