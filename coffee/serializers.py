9855c8c51b0bcf829ac9155ed19f16c6434b5e6d
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-08-26 03:06:06 +0900

    Update serializers.py

--- coffee/serializers.py
+++ coffee/serializers.py
@@ -2,7 +2,6 @@ from rest_framework import serializers
 from .models import Brand
 
 class BrandSerializer(serializers.ModelSerializer):
-    has_favorites = serializers.SerializerMethodField()
     class Meta:
         model = Brand
         fields = ["id","name"]
