4abb0e4d5b273c47f27e344ad2be222ba9ffb784
김지훈 <87347699+jomulagy@users.noreply.github.com>
2023-08-28 02:43:06 +0900

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -29,19 +29,7 @@ ALLOWED_HOSTS = []
 
 PROJECT_HOST = "http://3.36.95.105"
 # Application definition
-CORS_ALLOWED_ORIGINS = [
-    "http://127.0.0.1:3000"# 클라이언트의 주소
-]
-CORS_ALLOW_CREDENTIALS = True
 
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -84,6 +72,20 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://127.0.0.1:3000"# 클라이언트의 주소
+]
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_METHODS = [
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
 ]
 
 ROOT_URLCONF = 'config.urls'
